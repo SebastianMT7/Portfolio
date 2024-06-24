126c1bfe46b8a45ae097e5be56ef29512a6d6d6f
Sebastian Torney <STorney@web.de>
2024-06-24 21:19:04 +0200

    2r

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,20 +1,72 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  http = inject(HttpClient);
+
   name: string = '';
   email: string = '';
-  text: string = '';
+  message: string = '';
   allFieldsFilled: boolean = false;
 
-  checkFields() {
-    this.allFieldsFilled = this.name.trim() !== '' && this.email.trim() !== '' && this.text.trim() !== '';
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    checkbox: "",
   }
+
+  mailTest = true;
+
+
+  ngOnInit() {
+    this.checkFields();
+  }
+
+  checkFields() {
+    this.allFieldsFilled = this.name.trim() !== '' && this.email.trim() !== '' && this.message.trim() !== '';
+  }
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
+
 }
+
+
