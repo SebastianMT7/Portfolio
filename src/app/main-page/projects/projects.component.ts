87a0fb294ab8d86e107cf8413ca4df97d102d774
Sebastian Torney <STorney@web.de>
2024-08-04 19:25:41 +0200

    server links

--- src/app/main-page/projects/projects.component.ts
+++ src/app/main-page/projects/projects.component.ts
@@ -33,7 +33,7 @@ export class ProjectsComponent {
       skills: "Angular | TypeScript | HTML | CSS | Firebase",
       descriptionENG: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
       descriptionGER: "Aufgabenmanager basierend auf dem Kanban-System. Erstellen und organisieren Sie Aufgaben per Drag-and-Drop, und weisen Sie Benutzern und Kategorien zu.",
-      liveTest:"https://sebastian-torney.developerakademie.net/Join/index.html",
+      liveTest:"https://join.sebastian-torney.de",
       gitHub:"",
     },
 
@@ -42,7 +42,7 @@ export class ProjectsComponent {
       skills: "JavaScript | HTML | CSS",
       descriptionENG: "A simple Jump-and-Run game based on a object-oriented approach. Help Pepe to find coins and bottles to fight against the killer chicken.",
       descriptionGER: "Spring-, Lauf- und Wurfspiel basierend auf einem objektorientierten Ansatz. Hilf Pepe dabei, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.",
-      liveTest:"https://sebastian-torney.developerakademie.net/El-Pollo-Loco/index.html",
+      liveTest:"https://el-pollo-loco.sebastian-torney.de",
       gitHub:"https://github.com/SebastianMT7/El-Pollo-Loco",
     },
 
