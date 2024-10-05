fdd997abc5a17a99cb8f76539911942cfd80a412
Sebastian Torney <STorney@web.de>
2024-10-05 16:16:32 +0200

    backchange

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'info@sebastian-torney.de';  
+            $recipient = 'STorney@web.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
